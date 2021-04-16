ace639aaf0c2db985b5f6f667c02c3703a29d82a
ipa2800 <justin@outlook.com>
2021-04-16 16:37:04 +0800

    a

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import random
 
 API = 'http://api.xdaili.cn/xdaili-api//greatRecharge/getGreatIp?spiderId=cavnkfabvajfhv12fa1v4fv12g1ba2b1gsgb&orderno=vanfjvadfb1sg5b4s4bf4b1s2n&returnType=2&count=1'
 
-INIT_URL = 'http://www.dianping.com/beijing/ch10/p{}'
+INIT_URL = 'http://www.dianping.com/fuzhou/ch75/g34318p{}'
 CSS_URL = 'http://s3plus.meituan.net/v1/mss_0a06a471f9514fc79c981b5466f56b91/svgtextcss/f8198800680006c0424b3c7412023ee7.css'
 SVG_NUM_URL = 'http://s3plus.meituan.net/v1/mss_0a06a471f9514fc79c981b5466f56b91/svgtextcss/20b501902c483d49e1e66d2159f1d2b8.svg'
 SVG_FONT_URL = 'http://s3plus.meituan.net/v1/mss_0a06a471f9514fc79c981b5466f56b91/svgtextcss/37b6ad50c87b9ec0e1744f4291efb622.svg'
@@ -22,7 +22,7 @@ HEADERS = {
             'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6',
             'Cache-Control': 'max-age=0',
             'Host': 'www.dianping.com',
-            'Referer': 'http://www.dianping.com/beijing/',
+            'Referer': 'http://www.dianping.com/fuzhou/',
             'Upgrade-Insecure-Requests': '1',
             'User-Agent': random_ua,
             # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
